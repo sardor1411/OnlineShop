f70104d189af525d66aa7adf072634bbcd2ac23d
sardor1411 <shahnozka1104@gmail.com>
2025-03-10 13:11:07 +0500

    readme

---
+++ src/services/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDm5dpNNjAUc3vVJmbSpuYbgymB-S70tQw",
+  authDomain: "online-shop-cbb68.firebaseapp.com",
+  databaseURL: "https://online-shop-cbb68.firebaseio.com",
+  projectId: "online-shop-cbb68",
+  storageBucket: "online-shop-cbb68.appspot.com",
+  messagingSenderId: "485927190723",
+  appId: "1:485927190723:web:c9a3b74106f644e6302276",
+};
+
+export default firebaseConfig;
